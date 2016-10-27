df59fddadb942847a556b87b9d698cbb81b71dbe
Patrick Ford <patrickford@msn.com>
2016-10-27 00:24:20 -0700

    Basic operations setup

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 "use strict";
 
 $(document).ready(function() {
+  var expression = "";
 
+  function clear() {
+    expression = "";
+    $('.display').empty();
+  }
 
+  $('.button').click(function() {
+    var entry = $(this).text();
+    $('.display').append(entry);
+
+  })
 
 
 });
